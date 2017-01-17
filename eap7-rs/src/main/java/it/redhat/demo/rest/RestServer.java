6308bb7cc80a45d34be02ca8d7f2f04c653f7c7b
fax4ever <fabiomassimo.ercoli@gmail.com>
2017-01-17 17:56:35 +0100

    jax-rs log demo

--- eap7-rs/src/main/java/it/redhat/demo/rest/RestServer.java
+++ eap7-rs/src/main/java/it/redhat/demo/rest/RestServer.java
@@ -1,5 +1,8 @@
 package it.redhat.demo.rest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
@@ -9,9 +12,47 @@ import javax.ws.rs.Path;
 @Path("")
 public class RestServer {
 
+    private static Logger log = LoggerFactory.getLogger(RestServer.class);
+
     @GET
     public String ciao() {
         return "ciao";
+    }
+
+    @GET
+    @Path("debug")
+    public String debug() {
+
+        log.debug("invoke debug");
+        return "debug";
+
+    }
+
+    @GET
+    @Path("info")
+    public String info() {
+
+        log.info("invoke info");
+        return "info";
+
+    }
+
+    @GET
+    @Path("warn")
+    public String warn() {
+
+        log.warn("invoke warn");
+        return "warn";
+
+    }
+
+    @GET
+    @Path("error")
+    public String error() {
+
+        log.error("invoke error");
+        return "error";
+
     }
 
 }
